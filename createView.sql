13b5ab5b04b65f7a96d6205dc38dd30cf3d88e41
tong <bluedragon00000@gmail.com>
2023-10-05 16:56:39 +0800

    1005

--- createView.sql
+++ createView.sql
@@ -6,9 +6,10 @@ SELECT a.customerId,
            WHEN strftime('%m-%d', a.dateBack) < '07-01' THEN strftime('%Y', a.dateBack, '-1 year') || '/' ||
                                                              substr(strftime('%Y', a.dateBack), 3)
            ELSE strftime('%Y', a.dateBack) || '/' || substr(strftime('%Y', a.dateBack, '+1 year'), 3)
-           END                                           as taxYear,
-       sum(a.rentalCost)                                 as rentalCost
+           END                                      as taxYear,
+       sum(a.rentalCost)                            as rentalCost
 FROM rentalContract a
          JOIN Phone b USING (IMEI)
          JOIN PhoneModel c USING (modelNumber)
-WHERE a.dateBack IS NOT NULL;
+WHERE a.dateBack IS NOT NULL
+GROUP BY a.customerId, c.modelName, taxYear;
