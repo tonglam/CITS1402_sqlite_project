87f52661660588fc4d7811d18ece70a5bf360497
tong <bluedragon00000@gmail.com>
2023-10-05 16:58:03 +0800

    1005

--- createView.sql
+++ createView.sql
@@ -1,13 +1,13 @@
 CREATE VIEW CustomerSummary AS
 SELECT a.customerId,
        c.modelName,
-       sum(julianday(a.dateBack) - julianday(a.dateOut)) as daysRented,
+       sum(julianday(a.dateBack) - julianday(a.dateOut) + 1) as daysRented,
        CASE
            WHEN strftime('%m-%d', a.dateBack) < '07-01' THEN strftime('%Y', a.dateBack, '-1 year') || '/' ||
                                                              substr(strftime('%Y', a.dateBack), 3)
            ELSE strftime('%Y', a.dateBack) || '/' || substr(strftime('%Y', a.dateBack, '+1 year'), 3)
-           END                                      as taxYear,
-       sum(a.rentalCost)                            as rentalCost
+           END                                               as taxYear,
+       sum(a.rentalCost)                                     as rentalCost
 FROM rentalContract a
          JOIN Phone b USING (IMEI)
          JOIN PhoneModel c USING (modelNumber)
